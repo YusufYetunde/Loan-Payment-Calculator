b25d39421ce5648e688d2ad3e913560680e74685
Yusuf yetunde <yusufyetunde13@gmail.com>
2023-11-14 23:43:27 +0100

    u[pdate]

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ import { Header, Meta, NoSSR, TopGradient } from "@/components";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <NoSSR>
-      <div className="isolate bg-white mb-28">
+      <div className="isolate bg-white mb-28 ">
         <Meta />
         <TopGradient />
         <Header />
